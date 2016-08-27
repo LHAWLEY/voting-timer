5ef981134c389da200b6707d99db2312ee2007de
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2016-08-26 23:07:13 -0500

    Rows

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,19 @@ import {
   TouchableHighlight
 } from 'react-native';
 
+class Button extends Component {
+  render () {
+    return (
+      <TouchableHighlight
+        onPress={this.props.onClick}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>{this.props.text}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
 class Timer extends Component {
   toggleTimer () {
 
@@ -15,12 +28,11 @@ class Timer extends Component {
 
   render () {
     return (
-      <TouchableHighlight
-        onPress={this.toggleTimer.bind(this)}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Start</Text>
-      </TouchableHighlight>
+      <View style={styles.row}>
+        <Button text="Start" onClick={this.toggleTimer.bind(this)} />
+        <Button text="Clock" onClick={this.toggleTimer.bind(this)} />
+        <Button text="Vacant" onClick={this.toggleTimer.bind(this)} />
+      </View>
     );
   }
 }
@@ -33,15 +45,17 @@ class Header extends Component {
 
 class Timers extends Component {
   render () {
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const rows = ['Start', 'Start', 'Start'];
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
 
     return (
       <View style={styles.container}>
         <ListView
           renderHeader={() => <Header />}
-          dataSource={ds.cloneWithRows([''])}
+          dataSource={ds.cloneWithRows(rows)}
           renderRow={() => <Timer />}
-          contentContainerStyle={styles.timers}
         />
       </View>
     );
@@ -52,33 +66,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-    paddingTop: 30
-  },
-  timers: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch'
+    backgroundColor: '#aaa'
   },
   header: {
+    paddingTop: 30,
     fontSize: 14,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#eee',
     textAlign: 'center',
     borderBottomWidth: 12,
     borderBottomColor: '#444',
     fontWeight: 'bold'
   },
   button: {
-    backgroundColor: '#d6f3ff',
+    backgroundColor: '#2ECC71',
     height: 80,
+    flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'stretch',
+    marginTop: 2
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'stretch'
   },
   buttonText: {
-    fontSize: 24
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold'
   }
 });
 
